4c34905a87f2065e53d78053ef7b043e651e166d
logan-anderson <logan-anderson@users.noreply.github.com>
2020-08-18 18:43:52 +0000

    deploy: e050e7338cc5f71f73afbe8c774852be9ea97151

---
+++ example/precache-manifest.6f86ee1179e089ea3418f16ddace9a57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b60c6d6972f433ddce8e2b0ff0358c",
+    "url": "/react-tinacms-localization/example/index.html"
+  },
+  {
+    "revision": "49df19ed1c7fd13f3473",
+    "url": "/react-tinacms-localization/example/static/css/2.4568d01b.chunk.css"
+  },
+  {
+    "revision": "eca288d92b574c0a3d86",
+    "url": "/react-tinacms-localization/example/static/css/main.f907ef9e.chunk.css"
+  },
+  {
+    "revision": "49df19ed1c7fd13f3473",
+    "url": "/react-tinacms-localization/example/static/js/2.354fc2b0.chunk.js"
+  },
+  {
+    "revision": "75dc602eb7fa44348d2670a01d2e2d45",
+    "url": "/react-tinacms-localization/example/static/js/2.354fc2b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca288d92b574c0a3d86",
+    "url": "/react-tinacms-localization/example/static/js/main.b487dcd5.chunk.js"
+  },
+  {
+    "revision": "1ff1478be2ea063d8511",
+    "url": "/react-tinacms-localization/example/static/js/runtime-main.cf75141a.js"
+  }
+]);
